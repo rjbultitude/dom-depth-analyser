6cbf1df0fac7b8c373edcfa171703ac0637b12d4
Richard Bultitde <rbultitude@sapient.com>
2019-11-04 22:46:41 +0000

    remove console log

--- js/depth.js
+++ js/depth.js
@@ -14,18 +14,14 @@ export default function maxDepth(root) {
         const depthObj = maxDepth(child);
         // max = Math.max(max, maxDepth(child).max);
         if (max < depthObj.max) {
-            console.log('max is less than previous max', max);
-            console.log('max is less than previous max', depthObj.max);
             max = depthObj.max;
             deepestChildContainer = child;
         } else {
-            console.log('max is greater or equal to previous max', max);
             max = max;
             deepestChildContainer = depthObj.deepestChildContainer;
         }
         console.log('finished loop iteration for ', child);
     });
     console.log('returning. Max is', max);
-    console.log('returning. Deepest child container is', deepestChildContainer);
     return {max: max + 1, deepestChildContainer}
   };
